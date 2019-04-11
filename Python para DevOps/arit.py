f5988e88d8832db73a02a3cda497696a56554b6e
Lucas Gasparotto <lukasgasparotto@gmail.com>
2019-04-10 21:58:45 -0300

    Let's start to version these files

---
+++ Python para DevOps/arit.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*- 
+
+num1 = 5;
+
+num2 = 4;
+
+mult = num1 * num2; # Multiplicação
+
+adic = num1 + num2; # Adição (+)
+
+subt = num1 - num2; # Subtração
+
+divi = num1 / num2; # Divição
+
+modu = num1 % num2; # Módulo(%)
+
+print mult
+
+print adic
+
+print subt
+
+print divi
+
+print modu
